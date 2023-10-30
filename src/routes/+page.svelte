cdca880b3adf0eb1522d0fee72bb60d19eeb026a
Om Gandhi <omgandhi10@gmail.com>
2023-10-29 20:17:52 -0400

    Basic skeleton + .gitignore

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
 	import doodle1 from '$lib/images/socratica-doodle.png';
 	import doodle2 from '$lib/images/socratica-doodle-alt.png';
 	import people from '$lib/images/socratica-people.png';
+	import sessionPano from '$lib/images/session-pano.png';
 	import UsersThree from '$lib/images/UsersThree.png';
 	import MapTrifold from '$lib/images/MapTrifold.png';
 	import Eye from '$lib/images/Eye.png';
@@ -37,8 +38,7 @@
 
 <section>
 	<div class="flex flex-col h-screen justify-center bg-primary">
-		<div class="absolute top-5 left-10 w-10 h-10 bg-black rounded-full"></div>
-
+		<div class="absolute top-5 left-10 w-10 h-10 bg-black rounded-full" />
 		<div class="absolute top-5 right-5 flex flex-col space-y-2 items-end">
 			<button class="bg-white py-2 px-2.5 rounded-full border border-gray-300 hover:bg-gray-400 font-mono inline-flex items-center space-x-2 w-[105px]">
 				<img src={UsersThree} alt="Users" class="w-6 h-6" />
@@ -98,6 +98,18 @@
 	<div class="-mt-40">
 		<img src={people} alt="People at Socratica" class="w-full h-auto" />
 	</div>
+</section>
 
+<section class="flex flex-col items-center justify-center h-screen bg-gray-100 relative">
+  <div class="text-left absolute top-[15%] mx-36">
+	  <button class="bg-white px-6 py-3 rounded-full mb-6 text-sm uppercase font-mono border border-gray-300 hover:bg-gray-400">
+		<img src={Spiral} alt="Spiral" class="w-6 h-6 inline-block" />
+		who can come
+	</button>
+    <h1 class="text-4xl font-bold mb-4">Whether you're just starting out or far along, this is a place for <i>you</i>.</h1>
+    <p class="text-lg text-gray-700">We know it can be intimidating to start</p>
+	  <p class="text-lg text-gray-700 mb-8">(we've been there).</p>
+  </div>
+  <img src={sessionPano} alt="Your Description" class="absolute bottom-0 h-1/2 w-full object-cover">
 </section>
 

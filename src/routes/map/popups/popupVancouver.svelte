f89053594598e610de9ca5d7daebaf4ec6f630a8
Soham Basu <sohambasu963@gmail.com>
2023-11-10 00:33:25 -0500

    added more styling to map card

--- src/routes/map/popups/popupVancouver.svelte
+++ src/routes/map/popups/popupVancouver.svelte
@@ -1,5 +1,5 @@
 <div id="vancouver-popup" class="hidden px-4 py-8 flex flex-col items-center justify-center">
-    <div class="w-full text-left ml-4 mt-8">
+    <div class="w-full text-left mt-8">
         <h1 class="text-4xl text-black font-tiempos-headline">Atelier Vancouver</h1>
         <p class="text-#A09D98 font-bold font-graphik" style="font-size: 20px; color: #A09D98;">Since Sept 2023</p>
     </div>
@@ -9,7 +9,9 @@
         width="500"
         height="450"
         frameborder="0"
-        style="border: 1px solid #F5F1E2; border-radius: 8px; margin: 0 auto;"
+        style="border: 1.5px solid #EBE4D0; border-radius: 8px; margin: 0 auto;"
         aria-hidden="false"
     ></iframe>
 </div>
+
+

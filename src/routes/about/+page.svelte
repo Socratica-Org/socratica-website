f6863f515a4bf076a3299b02f62524d830997944
Soham Basu <sohambasu963@gmail.com>
2023-11-21 23:51:50 -0500

    fixed navbar buttons

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -9,19 +9,19 @@
 <div class="flex flex-col h-[820px] justify-center bg-primary">
     <div class="absolute top-5 left-7 sm:left-10 w-10 h-10 bg-black rounded-full" />
     <div class="absolute top-5 right-5 flex flex-col space-y-2 items-end">
-        <a href="/" class="transition bg-white md:bg-primary py-2 px-2.5 text-[10px] text-xs md:text-sm rounded-full border border-cool-grey hover:bg-cool-grey font-mono inline-flex items-center space-x-2">
+        <a href="/" class="bg-white md:bg-primary py-2 px-2.5 text-[10px] text-xs md:text-sm rounded-full border border-cool-grey hover:bg-cool-grey font-mono inline-flex items-center space-x-2 transition-colors duration-500 ease-in-out">
             <img src={DarkHouse} alt="home" class="w-4 h-4 md:w-6 md:h-6" />
             <span>HOME</span>
         </a>
-        <a href="/about" class="cursor-not-allowed opacity-50 transition bg-cool-grey py-2 px-2.5 text-[10px] text-xs md:text-sm rounded-full border border-cool-grey hover:bg-cool-grey font-mono inline-flex items-center space-x-2">
+        <a href="/about" class="cursor-not-allowed opacity-50 bg-cool-grey py-2 px-2.5 text-[10px] text-xs md:text-sm rounded-full border border-cool-grey hover:bg-cool-grey font-mono inline-flex items-center space-x-2 transition-colors duration-500 ease-in-out">
             <img src={UsersThree} alt="Users" class="w-4 h-4 md:w-6 md:h-6" />
             <span>ABOUT</span>
         </a>
-        <a href="/map" class="transition bg-white md:bg-primary py-2 px-2.5  text-[10px] text-xs md:text-sm rounded-full border border-cool-grey hover:bg-cool-grey font-mono inline-flex items-center space-x-2 ">
+        <a href="/map" class="bg-white md:bg-primary py-2 px-2.5 text-[10px] text-xs md:text-sm rounded-full border border-cool-grey hover:bg-cool-grey font-mono inline-flex items-center space-x-2 transition-colors duration-500 ease-in-out">
             <img src={MapTrifold} alt="Map" class="w-4 h-4 md:w-6 md:h-6" />
             <span>MAP</span>
         </a>
-        <a href="/get-involved" class="transition bg-white md:bg-primary py-2 px-2.5  text-[10px] text-xs md:text-sm rounded-full border border-cool-grey hover:bg-cool-grey font-mono inline-flex items-center space-x-2">
+        <a href="/get-involved" class="bg-white md:bg-primary py-2 px-2.5 text-[10px] text-xs md:text-sm rounded-full border border-cool-grey hover:bg-cool-grey font-mono inline-flex items-center space-x-2 transition-colors duration-500 ease-in-out">
             <img src={Eye} alt="Eye" class="w-4 h-4 md:w-6 md:h-6" />
             <span>GET INVOLVED</span>
         </a>

37d8c922ecd18aec84cb875c444051d49d1ae9f5
Replit user <>
2023-11-23 17:29:35 +0000

    fix: url nits

--- src/functions/subdomain.js
+++ src/functions/subdomain.js
@@ -1,11 +1,10 @@
-// src/functions/subdomain.js
-export async function redirectF23(req, res) {
+export default async function handler(req, res) {
   if (req.headers.host === 'f23.socratica.info') {
     res.writeHead(302, {
       Location: 'https://socratica.notion.site/Socratica-Symposium-F23-7183a4f10f704de1858c7f3894b368e9?pvs=4',
-    })
+    });
   } else {
-    res.writeHead(404)
+    res.writeHead(404);
   }
-  res.end()
+  res.end();
 }

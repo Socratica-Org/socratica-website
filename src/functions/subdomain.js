a38608d51a8d4a9f578b91c7fff19f6e54cdf9bd
Replit user <>
2023-11-23 17:34:29 +0000

    fix: url

--- src/functions/subdomain.js
+++ src/functions/subdomain.js
@@ -2,7 +2,7 @@ export default async function handler(req, res) {
   const subdomain = 'f23'; // Replace 'subdomain' with your desired subdomain
   const externalLink = 'https://socratica.notion.site/Socratica-Symposium-F23-7183a4f10f704de1858c7f3894b368e9?pvs=4'; // Replace with the external link you want to redirect to
 
-  if (req.headers.host === `${subdomain}.your-vercel-app-url.vercel.app`) {
+  if (req.headers.host === `${subdomain}.socratica.info`) {
     res.writeHead(302, { Location: externalLink });
     res.end();
   } else {

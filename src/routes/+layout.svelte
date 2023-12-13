86f9233ed313e7cb389b6e94571fc7a40b3e5df5
Hudhayfa Zaheem <hudzah@Hudhayfas-MacBook-Air-2.local>
2023-12-12 23:27:40 -0500

    Update internal.js and layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -132,7 +132,7 @@
 						<p class="leading-[26px]"><a  href="/about">About</a></p>
 						<p class="leading-[26px]"><a  href="/map">Map</a></p>
 						<p class="leading-[26px]"><a  href="/get-involved">Get Involved</a></p>
-						<p class="leading-[26px]"><a  href="https://www.garysu.ca/" target="_blank">Easter Egg</a></p>
+						<p class="leading-[26px]"><a  href="https://www.youtube.com/watch?v=ydYDqZQpim8" target="_blank">Easter Egg</a></p>
 						<p class="leading-[26px]"><a  href="https://socratica.notion.site/Code-of-Conduct-52fb005f7e3a452096e235995a2f92e5" target="_blank">Code of Conduct</a></p>
 						<p class="leading-[26px]"><a  href="https://donate.stripe.com/5kA6qZcondXE8Te008" target="_blank">Donate</a></p>
 					</div>

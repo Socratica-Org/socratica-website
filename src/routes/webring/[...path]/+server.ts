b4575c6e00ad8449a58fa2b6c60d322598b6fb22
Calvin Liang <me@calvin.sh>
2026-04-09 15:41:54 -0700

    fix webring proxy

--- src/routes/webring/[...path]/+server.ts
+++ src/routes/webring/[...path]/+server.ts
@@ -2,41 +2,8 @@ import type { RequestHandler } from "./$types";
 
 const ORIGIN = "https://webring.socratica-info.workers.dev";
 
-const proxy: RequestHandler = async ({ params, url, request }) => {
+export const fallback: RequestHandler = ({ params, url, request }) => {
   const target = `${ORIGIN}/webring/${params.path}${url.search}`;
-
-  const headers = new Headers(request.headers);
-  headers.delete("host");
-  headers.delete("connection");
-
-  const init: RequestInit = {
-    method: request.method,
-    headers,
-    redirect: "manual",
-  };
-
-  if (request.method !== "GET" && request.method !== "HEAD") {
-    init.body = await request.arrayBuffer();
-  }
-
-  const res = await fetch(target, init);
-
-  const resHeaders = new Headers(res.headers);
-  resHeaders.delete("content-encoding");
-  resHeaders.delete("content-length");
-  resHeaders.delete("transfer-encoding");
-
-  return new Response(res.body, {
-    status: res.status,
-    statusText: res.statusText,
-    headers: resHeaders,
-  });
+  const proxied = new Request(target, request);
+  return fetch(proxied);
 };
-
-export const GET = proxy;
-export const POST = proxy;
-export const PUT = proxy;
-export const PATCH = proxy;
-export const DELETE = proxy;
-export const HEAD = proxy;
-export const OPTIONS = proxy;
